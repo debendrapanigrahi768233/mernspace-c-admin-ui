8af67444251ee96d0f8b55f009996b4cf20dadf1
DEBENDRA PANIGRAHI <debbhubaneswar123@gmail.com>
2024-10-22 20:44:03 +0530

    Add protected routes

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,8 +1,14 @@
-import { Outlet } from "react-router-dom"
+import { Link, Navigate, Outlet } from "react-router-dom"
+import { userAuthStore } from "../store"
 
 const Dashboard = () => {
+    const {user} =userAuthStore()
+    if(user === null){
+        return <Navigate to='/auth/login' replace={true}/>
+    }
   return (
     <div>
+        <Link to='/auth/login'>Log In</Link>
         <h1>Here it will be injected, outlet is like children</h1>
         <Outlet/>
     </div>
--- src/layouts/NonAuth.tsx
+++ src/layouts/NonAuth.tsx
@@ -1,6 +1,12 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Navigate  } from "react-router-dom"
+import { userAuthStore } from "../store"
+
 
 const NonAuth = () => {
+    const {user} =userAuthStore()
+    if(user !== null){
+        return <Navigate to='/' replace={true}/>
+    }
   return (
     <div>
         <Outlet/>
