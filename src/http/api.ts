6321e973de2984db4407f0f8564cf295b7a6a046
DEBENDRA PANIGRAHI <debbhubaneswar123@gmail.com>
2024-12-01 19:54:42 +0530

    Add Users table

--- src/http/api.ts
+++ src/http/api.ts
@@ -10,3 +10,6 @@ export const self = () => api.get("/auth/self");
 
 //logout endpoint
 export const logout = () => api.post("/auth/logout");
+
+//Get users
+export const getUsers = () => api.get("/users");
--- src/pages/users/UserPage.tsx
+++ src/pages/users/UserPage.tsx
@@ -1,11 +1,65 @@
-import { Breadcrumb } from 'antd'
+import { Breadcrumb, Space, Table } from 'antd'
 import {RightOutlined} from '@ant-design/icons'
 import { Link } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import { getUsers } from '../../http/api'
+import { User } from '../../types'
+
+const columns = [
+  {
+    title: 'Id',
+    dataIndex : 'id',
+    key: 'id',
+  },
+  {
+    title: 'Name',
+    dataIndex : 'firstName',
+    key: 'firstName',
+    render: (_text: string, record : User)=> {
+      return(
+        <div>{record.firstName} {record.lastName}</div>
+      )
+      }
+  },
+  {
+    title: 'Email',
+    dataIndex : 'email',
+    key: 'email',
+  },
+  {
+    title: 'Role',
+    dataIndex : 'role',
+    key: 'role',
+  },
+  // {
+  //   title: 'Action',
+  //   dataIndex : 'action',
+  //   key: 'action',
+  //   render: ()=>{
+  //     return (
+  //       <div>
+  //         <Link to={`/users/edit`}>Edit</Link>
+  //       </div>
+  //     )
+  //   }
+  // }
+]
 
 const UserPage = () => {
+  const {data: users, isLoading, isError, error} = useQuery({
+    queryKey: ['users'],
+    queryFn:()=>{
+      return getUsers().then(response=>response.data)
+    }
+  })
   return (
   <>
-    <Breadcrumb separator={<RightOutlined />} items={[{ title: <Link to='/'>Dashboard</Link> }, { title: 'Users' }]} />
+    <Space size={'large'} direction='vertical' style={{width: '100%'}}>
+      <Breadcrumb separator={<RightOutlined />} items={[{ title: <Link to='/'>Dashboard</Link> }, { title: 'Users' }]} />
+      {isLoading && <>Loading...</>}
+      {isError && <>{error?.message}</>}
+      <Table dataSource={users} columns={columns} />
+    </Space>
   </>
   )
 }
