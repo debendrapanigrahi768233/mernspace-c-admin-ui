c75231caa319d4d23e28daed0a6993bcbfa1a4c4
DEBENDRA PANIGRAHI <debbhubaneswar123@gmail.com>
2025-01-25 19:16:28 +0530

    Add API Gateway

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,30 +1,34 @@
 import { CreateTenantData, CreateUserData, Credentials } from "../types";
 import { api } from "./client";
 
+export const AUTH_SERVICE = "/api/auth";
+// export const CATALOG_SERVICE = '/api/catalog'
+
 //Auth service----------------------------->
 export const login = (credentials: Credentials) =>
-  api.post("/auth/login", credentials);
+  api.post(`${AUTH_SERVICE}/auth/login`, credentials);
 
 //getself endpoint
-export const self = () => api.get("/auth/self");
+export const self = () => api.get(`${AUTH_SERVICE}/auth/self`);
 
 //logout endpoint
-export const logout = () => api.post("/auth/logout");
+export const logout = () => api.post(`${AUTH_SERVICE}/auth/logout`);
 
 //Get users
 export const getUsers = (queryString: string) =>
-  api.get(`/users?${queryString}`);
+  api.get(`${AUTH_SERVICE}/users?${queryString}`);
 
 //Get tenants
-export const getTenants = () => api.get("/tenants");
+export const getTenants = () => api.get(`${AUTH_SERVICE}/tenants`);
 
 //Create User
-export const createUser = (user: CreateUserData) => api.post("/users", user);
+export const createUser = (user: CreateUserData) =>
+  api.post(`${AUTH_SERVICE}/users`, user);
 
 //Update User
 export const updateUser = (user: CreateUserData, id: string) =>
-  api.patch(`/users/${id}`, user);
+  api.patch(`${AUTH_SERVICE}/users/${id}`, user);
 
 //Create tenant
 export const createTenant = (tenant: CreateTenantData) =>
-  api.post("/tenants", tenant);
+  api.post(`${AUTH_SERVICE}/tenants`, tenant);
