d33d3eb076e3a295f51e785b0868587a114ac1a6
DEBENDRA PANIGRAHI <debbhubaneswar123@gmail.com>
2025-01-25 19:25:30 +0530

    Add breadcrumbs and router for products page

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,7 @@ import NonAuth from "./layouts/NonAuth";
 import Root from "./layouts/Root";
 import Users from "./pages/users/Users";
 import Tenants from './pages/tenants/Tenants';
+import Products from "./pages/products/Products";
 
 
 export const router= createBrowserRouter([
@@ -30,6 +31,10 @@ export const router= createBrowserRouter([
                         path: '/restaurants',
                         element: <Tenants/>
                     },
+                    {
+                        path: '/products',
+                        element: <Products/>
+                    }
                 ]
             },
             {
